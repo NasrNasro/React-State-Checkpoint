1b741484cd376fb562d600b8dad16e7d40a47168
Nassreddine Hnana <nasrihnana@gmail.com>
2021-11-19 16:20:20 +0100

    React-State-Check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import photo from './PhotoNasr.jfif'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {Person: {fullName: "Nassreddine Hnana",
+      bio: "Im  FullStack  JS  intern  at  Gomycode",
+      imgSrc: photo,
+      profession: "Electric-Automatic Engineer"},
+      shows: true,
+      time: 0,
+    }
+  }
+
+  showState = (shows) => this.setState({shows: !shows})
+
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+    this.tock();
+  }
+  tick() {
+    this.setState({
+      time: this.state.time+1
+    });
+  }
+  tock() {
+    this.setState({
+      time: 0
+    });
+  }
+
+  render () {
+    console.log(this.state.shows)
+    console.log('render')
+    return(
+      <div className="App">
+        <button className = "stylebutton" onClick = { () => this.showState(this.state.shows) }>
+        {this.state.shows ? "Profile" : "Show profile"}
+        </button>
+        {() => this.handleTime(this.state.time)}
+        <h2>{this.state.shows ? this.state.time : null}</h2>
+        <div className="profile" style = {this.state.shows ? null: {display: "none"} }>
+          <img className = "stylePhoto" src={this.state.Person.imgSrc} alt = "PicNasr" />
+          <h1 className = "styleText">
+            Hello, my name is
+            <span className = "styleName"> {this.state.Person.fullName}</span>
+          </h1>
+          
+          <h2 className = "styleText">
+            My profession is : <span className = "styleProfession"> {this.state.Person.profession}</span>
+          </h2>
+          <h2>
+            <span className = "styleText"> {this.state.Person.bio}</span>
+          </h2>
+          <footer>
+            <p>Copyritghts &copy; {this.state.Person.fullName} 2021</p>
+          </footer>
+        </div>
+      </div>
+    )
+  } 
+};
 
 export default App;
